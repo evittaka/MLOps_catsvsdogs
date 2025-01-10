464febc713a2a5ab4025c8f90e43f302961c29c0
Bjarke Nilsson <bjarke@space.dtu.dk>
2025-01-10 12:11:37 +0100

    Added check for raw data and downloading data from kaggle if prompted

--- src/catsvsdogs/data.py
+++ src/catsvsdogs/data.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import typer
 from torch.utils.data import Dataset
 
+import os
 
 class MyDataset(Dataset):
     """My custom dataset."""
@@ -18,6 +19,32 @@ class MyDataset(Dataset):
 
     def preprocess(self, output_folder: Path) -> None:
         """Preprocess the raw data and save it to the output folder."""
+
+        # Check if data exists in folder
+        if check_if_data_exists(self.data_path) == False:
+            print("Data is missing from {}".format(self.data_path))
+            download_data_check = input("Download data from kaggle? [y/n]: ")
+            if download_data_check == "y":
+                print("Getting data from kaggle to {}".format(self.data_path))
+                download_data(self.data_path)
+
+def check_if_data_exists(raw_data_path: Path) -> None:
+    # Check if the dataset already exists
+    return os.path.exists(raw_data_path / "PetImages2")
+
+def download_data(raw_data_path: Path) -> None:
+    """ Get data from kaggle using kaggle api
+    """
+
+    # Get the dataset from kaggle
+    print("Using kaggle API key located in User/.../.kaggle")
+    import kaggle
+
+    # Authenticate kaggle API from Users/.../.kaggle
+    kaggle.api.authenticate()
+
+    # Get the dataset and put under data/raw
+    kaggle.api.dataset_download_files("shaunthesheep/microsoft-catsvsdogs-dataset", path=raw_data_path, unzip=True)
 
 def preprocess(raw_data_path: Path, output_folder: Path) -> None:
     print("Preprocessing data...")
