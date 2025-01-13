eb95b49891e0313799b71c58b4afe637dde31328
Bjarke Nilsson <bjarke@space.dtu.dk>
2025-01-13 13:13:03 +0100

    Added first unit tests for catsvsdogs data class

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,9 +1,10 @@
 from torch.utils.data import Dataset
 
-from catsvsdogs.data import MyDataset
+from catsvsdogs.data import catsvsdogs
 
 
-def test_my_dataset():
-    """Test the MyDataset class."""
-    dataset = MyDataset("data/raw")
-    assert isinstance(dataset, Dataset)
+def test_catsvsdogs():
+    """Test the catsvsdogs class."""
+    train_set, test_set = catsvsdogs()
+    assert isinstance(train_set, Dataset), "Train set is not a Torch.Dataset type"
+    assert isinstance(test_set, Dataset), "Test set is not a Torch.Dataset type"
