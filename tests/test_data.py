68d811fd9d5561ef9d569480e3897d82aa51b132
Bjarke Nilsson <bjarke@space.dtu.dk>
2025-01-16 14:08:24 +0100

    Added skip for data tests if data not available

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,7 +1,13 @@
 from catsvsdogs.data import catsvsdogs
 from torch.utils.data import Dataset
+from pathlib import Path
+import os
+import pytest
 
+# Statics
+RAW_DATA_PATH = Path("data/raw/PetImages")
 
+@pytest.mark.skipif(not os.path.exists(RAW_DATA_PATH), reason="Data files not found")
 def test_catsvsdogs():
     """Test the catsvsdogs class."""
     train_set, test_set = catsvsdogs()
