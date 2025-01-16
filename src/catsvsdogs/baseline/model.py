f99118bb3aeb8998f378ca20299c0ba06d0c6f35
Javier Moreno <javiermorenoprieto@gmail.com>
2025-01-16 10:39:59 +0100

    changed model to mobilenetv3

---
+++ src/catsvsdogs/baseline/model.py
@@ -0,0 +1,36 @@
+import torch
+from torch import nn
+
+
+class SimpleCNN(nn.Module):
+    """Simple CNN model."""
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.conv3 = nn.Conv2d(64, 128, 3, 1)
+        self.dropout = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(14 * 14 * 128, 2) # 14x14 image dimension on the last layer
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass."""
+        x = torch.relu(self.conv1(x))
+        x = torch.max_pool2d(x, 2, 2)
+        x = torch.relu(self.conv2(x))
+        x = torch.max_pool2d(x, 2, 2)
+        x = torch.relu(self.conv3(x))
+        x = torch.max_pool2d(x, 2, 2)
+        x = torch.flatten(x, 1)
+        x = self.dropout(x)
+        return self.fc1(x)
+
+
+if __name__ == "__main__":
+    model = SimpleCNN()
+    print(f"Model architecture: {model}")
+    print(f"Number of parameters: {sum(p.numel() for p in model.parameters())}")
+
+    dummy_input = torch.randn(1, 3, 128, 128)
+    output = model(dummy_input)
+    print(f"Output shape: {output.shape}")
