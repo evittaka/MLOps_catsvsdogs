95e27c5dbc60b8dc7f545b08b8a47a20d96313fa
Bjarke Nilsson <bjarke@space.dtu.dk>
2025-01-16 13:29:29 +0100

    Reworked test to use MobileNetV3

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,12 +1,12 @@
 import pytest
 import torch
-from catsvsdogs.model import SimpleCNN
+from catsvsdogs.model import MobileNetV3
 from torch import nn
 
 
 def test_model_setup():
     """Test the SimpleCNN model class."""
-    model = SimpleCNN()
+    model = MobileNetV3()
 
     # Check if model is an instance of nn.Module
     assert isinstance(model, nn.Module), "Model is not an instance of nn.Module"
@@ -14,7 +14,7 @@ def test_model_setup():
 # Test output for different batch sizes
 @pytest.mark.parametrize("batch_size", [1, 4, 8, 16, 32, 64, 128])
 def test_model_behaviour(batch_size: int) -> None:
-    model = SimpleCNN()
+    model = MobileNetV3()
 
     # Check data pipeline
     dummy_input = torch.randn(batch_size, 3, 128, 128)
