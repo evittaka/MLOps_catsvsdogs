aeaf79e4c39f8c789a74613a7dee6f0c495fc5ea
Javier Moreno <javiermorenoprieto@gmail.com>
2025-01-16 10:59:11 +0100

    Applied ruff

--- src/catsvsdogs/model.py
+++ src/catsvsdogs/model.py
@@ -1,10 +1,11 @@
 import timm
 from torch import nn
 
+
 class MobileNetV3(nn.Module):
     def __init__(self, pretrained: bool = True):
         super(MobileNetV3, self).__init__()
-        self.model = timm.create_model('mobilenetv3_large_100', pretrained=pretrained)
+        self.model = timm.create_model("mobilenetv3_large_100", pretrained=pretrained)
         self.model.classifier = nn.Linear(self.model.classifier.in_features, 2)
 
     def forward(self, x):
