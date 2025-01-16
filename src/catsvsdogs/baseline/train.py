19619c0beb76b5efd5f26d9e05bc732bec4eb155
Javier Moreno <javiermorenoprieto@gmail.com>
2025-01-16 10:39:59 +0100

    changed model to mobilenetv3

--- src/catsvsdogs/baseline/train.py
+++ src/catsvsdogs/baseline/train.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 import torch
 import typer
-from catsvsdogs.model import MobileNetV3
+from catsvsdogs.model import SimpleCNN
+
 from catsvsdogs.data import catsvsdogs
-from tqdm import tqdm
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 
@@ -13,7 +13,7 @@ def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10) -> None:
     print("Training model")
     print(f"{lr=}, {batch_size=}, {epochs=}")
 
-    model = MobileNetV3().to(DEVICE)
+    model = SimpleCNN().to(DEVICE)
     train_set, _ = catsvsdogs()
 
     train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size)
@@ -22,28 +22,25 @@ def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10) -> None:
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
     statistics = {"train_loss": [], "train_accuracy": []}
-
     for epoch in range(epochs):
         model.train()
-        progress_bar = tqdm(enumerate(train_dataloader), total=len(train_dataloader), desc=f"Epoch {epoch+1}/{epochs}")
-        for i, (img, target) in progress_bar:
+        for i, (img, target) in enumerate(train_dataloader):
             img, target = img.to(DEVICE), target.to(DEVICE)
             optimizer.zero_grad()
             y_pred = model(img)
             loss = loss_fn(y_pred, target)
             loss.backward()
             optimizer.step()
-
             statistics["train_loss"].append(loss.item())
+
             accuracy = (y_pred.argmax(dim=1) == target).float().mean().item()
             statistics["train_accuracy"].append(accuracy)
 
-            progress_bar.set_postfix({"loss": loss.item(), "accuracy": accuracy})
+            if i % 100 == 0:
+                print(f"Epoch {epoch}, iter {i}, loss: {loss.item()}")
 
     print("Training complete")
     torch.save(model.state_dict(), "models/model.pth")
-
-    # Save training statistics as a figure
     fig, axs = plt.subplots(1, 2, figsize=(15, 5))
     axs[0].plot(statistics["train_loss"])
     axs[0].set_title("Train loss")
